e043b826a7e5526c6a03485c37b5b2615a7f2d09
thangvynam1808@gmail.com <Namdeptrai12#>
2023-01-06 14:47:17 +0700

    init project

---
+++ src/main/java/com/nam/thang/transformkafka/TransformKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.nam.thang.transformkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransformKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransformKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nam/thang/transformkafka/TransformKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nam.thang.transformkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransformKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
