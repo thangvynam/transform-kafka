c16727c433b4186da8b9e41812e6c63f05894d15
thangvynam1808@gmail.com <Namdeptrai12#>
2023-01-07 16:04:06 +0700

    refactor: using log

--- src/main/java/com/nam/thang/transformkafka/consumer/service/MessageEventListener.java
+++ src/main/java/com/nam/thang/transformkafka/consumer/service/MessageEventListener.java
@@ -1,5 +1,6 @@
 package com.nam.thang.transformkafka.consumer.service;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -8,10 +9,11 @@ import org.springframework.stereotype.Service;
  * @since : 07/01/2023, Sat
  **/
 @Service
+@Log4j2
 public class MessageEventListener {
 
     @KafkaListener(topics = "${kafka.topic}", groupId = "${kafka.group-id}")
     public void receiveMessage(String message) {
-        System.out.println("Received Message in group foo: " + message);
+        log.info("Received Message in group foo: " + message);
     }
 }
